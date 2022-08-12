220ad994e0f31617201a837d002dbbca3a2bc2ee
skillnull <skillisnull@gmail.com>
2022-08-12 16:29:35 +0800

    1.1.0

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# 定义sed -i 参数(数组)
-# Default case for Linux sed, just use "-i"
-sedi=(-i)
-case "$(uname)" in
-  # For macOS, use two parameters
-  Darwin*) sedi=(-i "")
-esac
 
 rm -rf dist
 
@@ -14,7 +7,5 @@ cp device.js device.export.js
 echo 'export default Device;' >> device.export.js
 
 babel index.js device.js device.export.js -d dist
-
-#sed "${sedi[@]}" "/export default Device;/d" device.js
 
 rm device.export.js
