82487b17128221ffefb8fae345973fbf6edb00a0
skillnull <skillisnull@gmail.com>
2024-10-31 15:47:05 +0800

    feat: homepage

--- src/index.js
+++ src/index.js
@@ -8,20 +8,19 @@ let proxy = new Proxy({}, {
     if (result) {
       Device.Info({
         domain: 'https://www.skillnull.com',
-        // transferDateToLunar: '2023/1/30',
         info: value && value.split(',')
       }).then(infoResult => {
         let infoHtml = []
         for (let i in infoResult) {
-          let value =  infoResult[i]
-          if (i === 'lunarDate'){
+          let value = infoResult[i]
+          if (i === 'lunarDate') {
             value = `${infoResult[i].year} ${infoResult[i].chineseZodiac} ${infoResult[i].month}${infoResult[i].day}`
           }
           infoHtml.push(
             '<li>' +
             '   <span>' + i + '</span>' +
             '   <span style="margin:0 1px;">:</span>' +
-            '   <span style="color: red;">' + value + '</span>' +
+            '   <span style="color: #3d8dbc;">' + value + '</span>' +
             '</li>')
         }
         document.querySelector('#info_box').innerHTML = '<ul style="margin: 5px;">' + infoHtml.join('') + '</ul>'
@@ -37,3 +36,20 @@ let proxy = new Proxy({}, {
 const getInfo = () => {
   proxy.value = document.querySelector('#info_input').value
 }
+
+const fork = () => {
+  window.open('https://github.com/skillnull/DeviceJs')
+}
+
+const setTopDistance = () => {
+  const title = document.querySelector('.title')
+  const title_height = title.getBoundingClientRect().height
+  const keyTipBox = document.querySelector('.content')
+  keyTipBox.style.paddingTop = title_height + 'px'
+}
+
+setTopDistance()
+
+window.addEventListener('resize', () => {
+  setTopDistance()
+})
